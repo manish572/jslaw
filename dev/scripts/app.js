849c77f537c384af93cabb64e8ed7c892cacadc9
Manish Sabharwal <manish57200@gmail.com>
2018-11-15 10:54:14 -0500

    Primary website established

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,10 @@
-
+// // Initialize and add the map
+// function initMap() {
+//    // The location of Uluru
+//    var uluru = {lat: 43.655604, lng: -79.385946};
+//    // The map, centered at Uluru
+//    var map = new google.maps.Map(
+//        document.getElementById('map'), {zoom: 15, center: uluru});
+//    // The marker, positioned at Uluru
+//    var marker = new google.maps.Marker({position: uluru, map: map});
+//  }
