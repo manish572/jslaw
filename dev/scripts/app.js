6a3d924e85ca40fd0e28583de0ab939706afc325
Manish Sabharwal <manish57200@gmail.com>
2019-01-01 12:36:51 -0500

    final responsive version

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -8,3 +8,37 @@
 //    // The marker, positioned at Uluru
 //    var marker = new google.maps.Marker({position: uluru, map: map});
 //  }
+
+'use strict';
+
+//smooth scroll starts
+// Select all links with hashes
+$('a[href*="#"]').click(function (event) {
+  {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 500);
+    }
+  }
+});
+//smooth scroll ends
+
+
+//Scroll Magic code
+var controller = new ScrollMagic.Controller();
+
+var scrollMagicScene = function scrollMagicScene(elementID, classToAdd) {
+  var newScene = new ScrollMagic.Scene({
+    triggerElement: elementID,
+    offset: -200
+  }).setClassToggle(elementID, classToAdd).addTo(controller);
+};
+
+scrollMagicScene("#serviceBoxInfo", "piece-fade-in");
